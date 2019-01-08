3ea7242bb640aec56aba542e332c509a244e90f2
yhalb <pyhalb@gmail.com>
2019-01-08 21:39:11 +0100

    popovers for entries

--- js.js
+++ js.js
@@ -1,4 +1,22 @@
 (function () {
+    $('.entry-text a').each(function() {
+        var $link = $(this);
+        var href = $link.attr('href')
+        if (href[0] == '#') {
+            $entry = $(href);
+
+            $link.attr('data-toggle', 'popover');
+            $link.attr('data-content', $entry.find('.entry-text').text());
+            $link.attr('title', $entry.find('.card-header').text())
+            $link.attr('data-trigger', 'hover');
+            $link.attr('data-html', 'true');
+
+        }
+    });
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
+
     window.onscroll = function() {handleTopButton()};
     function handleTopButton() {
         $("#button-top").toggle(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100);
